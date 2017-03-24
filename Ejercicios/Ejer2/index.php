58c2142a7097b83d084ddebfedac12cfef0ccb9a
BrunoDanielPretti <alynya@gmail.com>
2017-03-24 17:07:35 -0300

    Ejercicio 2 listo

--- Ejercicios/Ejer2/index.php
+++ Ejercicios/Ejer2/index.php
@@ -2,4 +2,32 @@
 
 echo date("d")."/".date("m")."/".date("y");
 
+
+$dia = date('z');
+$dia = 170;
+    
+    if ( $dia < 79 )
+    {
+        $estacion = 'Verano';    
+    } 
+    elseif ( $dia < 172 )
+    {
+        $estacion = 'Otoño'; 
+    }
+    elseif ( $dia < 265 )
+    {
+        $estacion = 'Invierno';
+    }
+    elseif ( $dia < 352 )
+    {
+        $estacion = 'Primavera';
+    } 
+    else 
+    {
+        $estacion = 'Verano';
+    }
+
+echo "<br>Estacion: ".$estacion;    
+
 ?>
+
