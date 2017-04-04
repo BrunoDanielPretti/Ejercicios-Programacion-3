337ad8710d5fd296306f67c7134919aca5019af7
alumno <alumno>
2017-04-04 20:59:52 -0300

    No tengo idea de lo q estoy haciendo

---
+++ TP/fabrica.php
@@ -0,0 +1,127 @@
+<?php
+
+    class Fabrica
+    {
+        private $_empleados;
+        private $_razonSocial;
+
+        public function __construct($pRazonSocial)
+        {
+            $this->_razonSocial = $pRazonSocial;
+            $this->_empleados = array();
+        }
+
+        public function AgregarEmpleado($pPersona)
+        {
+            array_push($this->_empleados, $pPersona);
+        }
+
+        public function CalcularSueldos()
+        {}
+
+        public function EliminarEmpleado($pEmpleado)
+        {
+            /*
+            foreach ($this->_empleados as $key)
+            {
+                if($key->getLegajo() == $pEmpleado->getLegajo() )
+                {
+                    unset( $this->_empleados[$key] );
+                    echo "eliminado<br>";
+                }
+            }
+            */
+
+         
+
+            for($i=0; $i<count($this->_empleados); $i++)
+            {
+                if($this->_empleados[$i]->getLegajo() == $pEmpleado->getLegajo() )
+                {
+                    unset( $this->_empleados[$i] );
+                    echo "eliminado<br>";
+                }
+            }
+        }
+
+        private function EliminarEmpleadosRepetidos()
+        {}
+
+        public function ToString()
+        {
+            
+            echo "Razon Social: $this->_razonSocial<br>";
+            foreach ($this->_empleados as $key)
+            {
+                echo $key->ToString();
+                echo "<br>";
+            }
+            
+            /*
+            $String1 = "";
+            foreach ($this->_empleados as $key)
+            {
+                $String1 = $String1.$key->ToString()."<br";                
+            }
+            return $String1;
+            */
+        }
+
+        public function GuardarFabrica()
+        {
+            $fichero = fopen("fabrica.txt", "w");
+            foreach ($this->_empleados as $key)
+            {
+                fwrite($fichero , $key->ToString()."\r\n" );                
+            }
+            fclose($fichero);
+            
+        }
+
+        public function TraerFabrica()
+        {
+            $file = fopen("fabrica.txt", "r");
+            $miArray = array();
+
+            /*
+            while(!feof($file))
+            {
+                $linea = fgets($file)."<br>";
+                array_push($miArray, explode(" - ", $linea) ) ;                
+            }
+            fclose($file);
+            
+            echo "<br><br>";
+            var_dump($miArray);
+            */
+                
+            while(!feof($file))
+            {
+                $linea = fgets($file)."<br>";
+                $miArray = explode(" - ", $linea) ;                
+                var_dump($miArray);
+                echo "<br>";
+            }
+            echo "<br><br>";
+            
+            fclose($file);    
+            
+        }
+
+        
+        public static function ImprimirArchivo()
+        {
+            $file = fopen("fabrica.txt", "r");
+
+            while(!feof($file))
+            {
+                echo fgets($file)."<br>";
+            }
+            fclose($file);
+        }
+        
+
+
+
+    }
+?>
--- TP/index.php
+++ TP/index.php
@@ -1,9 +1,29 @@
 <?php
     require_once "persona.php";
     require_once "empleado.php";
+    require_once "fabrica.php";
 
     $miPersona = new empleado("Bruno", "Pretti", 36806053, 'm', 10, 1000);
+    $per1 = new empleado("Marina", "Meza", 12053554, 'm', 20, 2000);
+    $per2 = new empleado("Puchi", "Perro", 33333333, 'm', 30, 3000);
+    $per3 = new empleado("Ñandu", "Perez", 44444444, 'm', 40, 4000);
     echo $miPersona->ToString()."<br>";
     echo $miPersona->Hablar("español");
-    echo "<br>asd";
+    echo "<br>asd<br><br>";
+
+    $miFabrica = new Fabrica("Le Fabrica");
+    $miFabrica->AgregarEmpleado($miPersona);
+    $miFabrica->AgregarEmpleado($per1);
+    $miFabrica->AgregarEmpleado($per2);
+    $miFabrica->AgregarEmpleado($per3);
+
+    $miFabrica->ToString();
+    //$miFabrica->EliminarEmpleado($miPersona);
+    //$miFabrica->ToString();
+
+    //$miFabrica->GuardarFabrica();
+
+    echo "------------------------------------<br>";
+    //Fabrica::ImprimirArchivo();
+    $miFabrica->TraerFabrica();
 ?>
